b7e49da3b2b36053854b9035c27d29693d1e9434
Raden-nvii <navinugraha15@gmail.com>
2025-01-13 16:08:52 +0800

    feat: change models file name

--- models/database_config.py
+++ models/database_config.py
@@ -1,15 +1,36 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Text, Float, Date, TIMESTAMP, JSON
+from sqlalchemy.sql import func
+from models.database_init import Base
 
-load_dotenv()
+class Invoice(Base):
+    __tablename__ = "invoices"
+    id = Column(Integer, primary_key=True, index=True)
+    invoice_number = Column(String(50), nullable=False, unique=True)
+    invoice_type_id = Column(Integer, foreign_key=True, nullable=True)
+    unified_number = Column(String(50), nullable=True)
+    issue_date = Column(Date, nullable=True)
+    items_id = Column(Integer, foreign_key=True)
+    total_before_tax = Column(Float, nullable=True)
+    tax = Column(Float, nullable=True)
+    total_after_tax = Column(Float, nullable= True)
+    created_at = Column(TIMESTAMP, nullable=False, default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db_session():
-    return SessionLocal()
+class InvoiceItems(Base):
+    __tablename__ = "invoice_items"
+    id = Column(Integer, primary_key=True, index=True)
+    invoice_id = Column(Integer, nullable=False)
+    item_name = Column(String(255), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    unit_price = Column(Float, nullable=False)
+    amount = Column(Float, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=func.now())
+    updated_at = Column(TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
+    
+    
+    class InvoiceType(Base):
+        __tablename__ = "invoice_types"
+        id = Column(Integer, primary_key=True, index=True)
+        name = Column(String(50), nullable=False)
+        created_at = Column(TIMESTAMP, nullable=False, default=func.now())
+        updated_at = Column(TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
