2f6abb3ec8307b5f9002d2e199a16855dec4880c
Raden-nvii <navinugraha15@gmail.com>
2025-01-13 15:41:55 +0800

    feat: change database and migration

--- migration/env.py
+++ migration/env.py
@@ -2,23 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from models.database_config import Base 
-from models.invoice_model import Invoice
-from dotenv import load_dotenv
-import os
-
-# Load .env file if present
-load_dotenv()
-
-# Get the DATABASE_URL environment variable
-database_url = os.getenv("DATABASE_URL")
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -29,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
