ee09654c125e6888c68d5ecafc7a33033b540f1d
Raden-nvii <navinugraha15@gmail.com>
2025-01-13 16:25:37 +0800

    feat: add foreign key on the invoice items model

--- models/invoice_items_model.py
+++ models/invoice_items_model.py
@@ -5,7 +5,7 @@ from models.database_init import Base
 class InvoiceItems(Base):
     __tablename__ = "invoice_items"
     id = Column(Integer, primary_key=True, index=True)
-    invoice_id = Column(Integer, nullable=False)
+    invoice_id = Column(Integer, foreign_key=True,nullable=False)
     item_name = Column(String(255), nullable=False)
     quantity = Column(Integer, nullable=False)
     unit_price = Column(Float, nullable=False)
