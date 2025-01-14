e54db7c4f520766ada6409a36326a1b5ee967a9d
Raden-nvii <navinugraha15@gmail.com>
2025-01-15 01:03:09 +0800

    modify streamlit app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from services.structured_extraction import extract_text
 from services.structured_parse import parse_invoice_data
 from services.database_service import store_invoice_data, get_recent_invoices
 from models.database_config import get_db_session  
+from services.preprocess_image import preprocess_image
 import os
 from pdf2image import convert_from_path
 from PIL import Image
+import cv2
 
 # Load environment variables
 load_dotenv()
@@ -43,9 +45,34 @@ st.download_button(
 
 if uploaded_file is not None and not st.session_state.processed:
     try:
-        # Extract text using Azure OCR
-        structured_data = extract_text(uploaded_file)
-        print("streamlit structured data:", structured_data)
+        
+        if uploaded_file.type == "application/pdf":
+            # process the pdf file
+            images = convert_from_path(uploaded_file.name)
+            
+            for i, page in enumerate(images):
+                image_path = os.path.join("uploads", f'page_{i + 1}.jpg')
+                page.save(image_path, 'JPEG')
+                image = cv2.imread(image_path)
+                preprocessed_image = preprocess_image(image)
+                preprocessed_image_path = image_path.replace('.jpg', '_preprocessed.jpg')
+                cv2.imwrite(preprocessed_image_path, preprocessed_image)
+                
+                with open(preprocessed_image_path, "rb") as image_file:
+                    structured_data = extract_text(image_file)
+                
+                
+        elif uploaded_file.type == ["image/jpeg", "image/png"]:
+            image = Image.open(uploaded_file)
+            preprocessed_image = preprocess_image(image)
+            preprocessed_image_path = uploaded_file.name.replace('.jpg', '_preprocessed.jpg').replace('.jpeg', '_preprocessed.jpeg').replace('.png', '_preprocessed.png')
+            cv2.imwrite(preprocessed_image_path, preprocessed_image)
+            
+            with open(preprocessed_image_path, "rb") as image_file:
+                structured_data = extract_text(image_file)
+        else:
+            st.error("Unsupported file format.")
+            st.stop()
 
         # Parse the structured data
         parsed_data = parse_invoice_data(structured_data)
